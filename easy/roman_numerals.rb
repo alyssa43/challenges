df4666cde1a8ff20cd72be314d3698d62b06a1e7
Alyssa Easter <AlyssaStarley@gmail.com>
2024-09-05 14:04:24 -0700

    Refactored Roman Numerals challenge

--- easy/roman_numerals.rb
+++ easy/roman_numerals.rb
@@ -1,5 +1,5 @@
 class RomanNumeral
-  @@values = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C',
+  VALUES = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C',
                90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V',
                4 => 'IV', 1 => 'I' }
 
@@ -11,7 +11,7 @@ class RomanNumeral
     result = ''
     num = @number
 
-    @@values.each do |int, roman_num|
+    VALUES.each do |int, roman_num|
       while num >= int
         result << roman_num
         num -= int
